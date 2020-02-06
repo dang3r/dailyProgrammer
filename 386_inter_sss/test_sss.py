9159110eefe02706929b013ff39db8cdaece455e
Daniel Cardoza <dan@danielcardoza.com>
2020-02-05 22:46:24 -0800

    prob: Solve #386-intermediate Single Symbol Squares with backtracking
    Use backtracking to solve this. Added tests for validate because it was
    broken.
    
    Solves : #386-intermediate

--- 386_inter_sss/test_sss.py
+++ 386_inter_sss/test_sss.py
@@ -10,6 +10,11 @@ https://www.reddit.com/r/dailyprogrammer/comments/9z3mjk/20181121_challenge_368_
     - eg. start of with all 0s. Try to flip things squares and if it decreases
         the number of errors, proceed down the path
     - if nowhere to proceed, backtrack
+- I used https://www.youtube.com/watch?v=Zq4upTEaQyM to help implement the backtracking approach. My key takeaways are
+    - how you iterate through a grid solution is important. The trick to go to the next row
+        if you go past the last column is nice.
+    - proceed if the decision you made is valid. If not, try another decision.
+        - decisions can be naive, the validation logic is what is complex
 """
 
 
@@ -19,12 +24,11 @@ def square(n :int) -> str:
     for i, j in product(range(n), repeat=2):
         h = 1
         while i + h < n and j + h < n:
-            corners = [bools[i][j], bools[i+1][j], bools[i][j+1], bools[i+1][j+1]]
+            corners = [bools[i][j], bools[i+h][j], bools[i][j+h], bools[i+h][j+h]]
             _sum = Sum([If(corner, 1, 0) for corner in corners])
             s.add(_sum != 0, _sum != 4)
             h += 1
 
-    print("LOL")
     d = ""
     if s.check() == sat:
         m = s.model()
@@ -37,19 +41,75 @@ def square(n :int) -> str:
             d += "\n"
     return d
 
+def square_bt(n: int):
+    if n == 1:
+        return "X"
+
+    d = [["" for j in range(n)] for i in range(n)]
+
+    def valid():
+        for i, j in product(range(n), repeat=2):
+            h = 1
+            while i + h < n and j + h < n:
+                items = [d[i][j], d[i+h][j], d[i][j+h], d[i+h][j+h]]
+                if len(set(items)) == 1 and len(items[0]) == 1:
+                    return False
+                h += 1
+        return True
+
+    def solve(i: int, j: int):
+        if j == n:
+            i += 1
+            j = 0
+            if i == n:
+                return True
+
+        for num in "XO":
+            d[i][j] = num
+            if valid() and solve(i, j+1):
+                return True
+            d[i][j] = ""
+
+        return False
+
+    
+    assert solve(0, 0) == True
+    return "\n".join(["".join(line) for line in d])
+
 
 def validate(s):
     d = [line for line in s.splitlines()]
     n = len(d)
-    print(d, n)
     for i, j in product(range(n), repeat=2):
         h = 1
         while i + h < n and j + h < n:
-            items = [d[i][j], d[i+1][j], d[i][j+1], d[i+1][j+1]]
-            if set(items) == 1:
+            items = [d[i][j], d[i+h][j], d[i][j+h], d[i+h][j+h]]
+            if len(set(items)) == 1:
                 return False
             h += 1
     return True
+
+
+
+def test_validate():
+    assert not validate(
+"""0110
+1111
+1111
+0110""")
+    assert validate(
+"""OOOXX
+XXOOO
+OOXOX
+OXOOX
+XOXXO""")
+
+def test_1():
+    assert validate(square_bt(2))
+    assert validate(square_bt(3))
+    assert validate(square_bt(4))
+    assert validate(square_bt(5))
+    assert validate(square_bt(6))
 
 def test_2():
     assert validate(square(2))
